1700e08b3aca326daa11463a01fce94b71d1d494
Yago Pessoa <yago.pessoa@sienaidea.com.br>
2018-10-31 17:42:58 -0300

    Project created

--- src/App.js
+++ src/App.js
@@ -1,27 +1,80 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import 'typeface-roboto'
+import './App.css'
+
+import theme from './theme'
+import { MuiThemeProvider, Typography } from '@material-ui/core'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Button from '@material-ui/core/Button'
+
+import Games from '@material-ui/icons/Games'
+import GroupAdd from '@material-ui/icons/GroupAdd'
+
+const styles = {
+  root: {
+    flexGrow: 1,
+    width: '100%'
+  },
+  toolbar: {
+    flexGrow: 1,
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  icon: {
+    marginRight: 8,
+  },
+  center: {
+    flexGrow: 1
+  },
+  image: {
+    height: '70%',
+    padding: 16,
+  },
+  nameLogo: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <header className="App-header">
+            <div style={styles.root}>
+              <AppBar color="primary" position="static">
+                <Toolbar style={styles.toolbar}>
+                  <div>
+                    <Button color="inherit">
+                      <Games style={styles.icon} />
+                      Nossos jogos
+                    </Button>
+                    <Button color="inherit">
+                      <GroupAdd style={styles.icon} />
+                      Processo Seletivo
+                    </Button>
+                  </div>
+                </Toolbar>
+              </AppBar>
+            </div>
+
+            <div style={styles.center}>
+              <div style={styles.nameLogo}>
+                <img style={styles.image} src={require('./Images/brasao.png')} alt="Brasão" />
+                <Typography variant="h1">Fellowship of the Game</Typography>
+              </div>
+            </div>
+
+          </header>
+        </div>
+      </MuiThemeProvider>
+    )
   }
 }
 
---
+++ src/theme.js
@@ -0,0 +1,10 @@
+import { createMuiTheme } from '@material-ui/core'
+
+const theme = createMuiTheme({
+    palette: {
+        primary: { main: '#1E2264' },
+        secondary: { main: '#F1CD36' },
+    },
+})
+
+export default theme
