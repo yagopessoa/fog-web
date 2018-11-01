00755e806de61f82332ab891ed9a7187cd8e3934
Yago Pessoa <yagorp15@gmail.com>
2018-11-01 01:03:49 -0300

    Initial setup of the starting page

--- src/App.js
+++ src/App.js
@@ -10,10 +10,18 @@ import Button from '@material-ui/core/Button'
 
 import Games from '@material-ui/icons/Games'
 import GroupAdd from '@material-ui/icons/GroupAdd'
+import Radio from '@material-ui/icons/Radio'
+import Info from '@material-ui/icons/Info'
+
+/* import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom' */
 
 const styles = {
   root: {
-    flexGrow: 1,
     width: '100%'
   },
   toolbar: {
@@ -29,7 +37,9 @@ const styles = {
     marginRight: 8,
   },
   center: {
-    flexGrow: 1
+    flexGrow: 1,
+    display: 'flex',
+    alignItems: 'center',
   },
   image: {
     height: '70%',
@@ -39,6 +49,19 @@ const styles = {
     display: 'flex',
     alignItems: 'center',
   },
+  titles: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 32,
+  },
+  button: {
+    marginTop: 48,
+  },
+  appBarButton: {
+    marginLeft: 16,
+  }
 }
 
 class App extends Component {
@@ -47,15 +70,24 @@ class App extends Component {
       <MuiThemeProvider theme={theme}>
         <div className="App">
           <header className="App-header">
+
             <div style={styles.root}>
               <AppBar color="primary" position="static">
                 <Toolbar style={styles.toolbar}>
                   <div>
-                    <Button color="inherit">
+                    <Button style={styles.appBarButton} color="inherit">
+                      <Info style={styles.icon} />
+                      Sobre
+                    </Button>
+                    <Button style={styles.appBarButton} color="inherit">
                       <Games style={styles.icon} />
                       Nossos jogos
                     </Button>
-                    <Button color="inherit">
+                    <Button style={styles.appBarButton} color="inherit">
+                      <Radio style={styles.icon} />
+                      Podcast
+                    </Button>
+                    <Button style={styles.appBarButton} color="inherit">
                       <GroupAdd style={styles.icon} />
                       Processo Seletivo
                     </Button>
@@ -67,7 +99,13 @@ class App extends Component {
             <div style={styles.center}>
               <div style={styles.nameLogo}>
                 <img style={styles.image} src={require('./Images/brasao.png')} alt="Brasão" />
-                <Typography variant="h1">Fellowship of the Game</Typography>
+                <div style={styles.titles}>
+                  <Typography style={{fontSize: '4em'}} variant="h1" gutterBottom>Fellowship of the Game</Typography>
+                  <Typography style={{fontSize: '1.4em'}} variant="h4" gutterBottom>Grupo de extensão de desenvolvimento de jogos - ICMC - USP</Typography>
+                  <Button style={styles.button} variant="contained" color="secondary" size="large" >
+                    Saiba mais
+                  </Button>
+                </div>
               </div>
             </div>
 
